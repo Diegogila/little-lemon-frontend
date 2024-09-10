b63054e24f7847798c34cd2db35e4629a455a1bf
diegogil <dgg26496@hotmail.com>
2024-09-10 17:58:15 -0600

    I added meta tags and semantic tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
 
 
 function App() {
-  return <div>Homepage</div>
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from './Navbar'
+
+function Header(){
+    return (
+        <header>
+            <img src="" alt="logo"/>
+            <Navbar/>
+        </header>
+    )
+}
+
+export default Header;
