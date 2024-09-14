588af08ac8dfbc7d459452777e7b2b8f94778ba3
diegogil <dgg26496@hotmail.com>
2024-09-14 13:32:36 -0600

    Add mobile responsive to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,27 @@ import React from "react";
 import Navbar from '../Navbar/Navbar'
 import './Header.module.css'
 import logo from '../../assets/images/Logo_header.svg'
+import { NavMenu } from "../NavMenu/NavMenu";
+
 
 function Header(){
+    const [isMobile, setIsMobile] = React.useState(false)
+    React.useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 991 ? true : false)
+            console.log(isMobile)
+        }
+
+        window.addEventListener('resize',handleResize);
+        return () => {
+            window.removeEventListener('resize',handleResize)
+        }
+    },[])
     return (
         <header>
-            <Navbar/>
+            {isMobile && <Navbar/>}
             <img src={logo} alt="logo"/>
+            {!isMobile && <NavMenu/>}
         </header>
     )
 }
