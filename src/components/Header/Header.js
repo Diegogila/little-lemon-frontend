697cae9f1270012bc432364152f40e0380c3f9e4
diegogil <dgg26496@hotmail.com>
2024-09-27 17:40:32 -0600

    Correct

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,17 @@ import './Header.css'
 import logo from '../../assets/images/Logo_header.svg'
 import { NavMenu } from "../navs/NavMenu/NavMenu";
 import GoBack from "../navs/GoBack/Goback";
+import { useNavigate } from "react-router-dom";
 
 
 function Header({isSection=false}){
     const [displayWidth, setDisplayWidth] = React.useState(0)
-
+    const navigate = useNavigate();
     const isMobile = () => displayWidth < 991 ? true : false;
+
+    const handleClick = () => {
+        navigate('/');
+      }
 
     React.useEffect(() => {
         setDisplayWidth(window.innerWidth)
@@ -25,7 +30,7 @@ function Header({isSection=false}){
     return (
             <header className={isSection ? "is-section" : "is-home"}>
                 {isSection && <GoBack/> }
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" onClick={handleClick}/>
                 {isMobile() ? <Navbar/> : <NavMenu/>}
             </header>
     )
